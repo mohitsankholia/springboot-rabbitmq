06b848b8265562eb8387237179344dd89875dfc5
mohitsankholia <sankholia.mohit@gmail.com>
2023-05-11 13:30:18 +0530

    rabbitmq for string message

---
+++ src/main/java/com/mohit/rabbitmq/consumer/RabbitmqConsumer.java
@@ -0,0 +1,18 @@
+package com.mohit.rabbitmq.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitmqConsumer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RabbitmqConsumer.class);
+
+	@RabbitListener(queues = { "${rabbitmq.queue.name}" })
+	public void consume(String message) {
+		LOGGER.info(String.format("Received message -> %s", message));
+	}
+
+}
---
+++ src/main/java/com/mohit/rabbitmq/producer/RabbitMqProducer.java
@@ -0,0 +1,29 @@
+package com.mohit.rabbitmq.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMqProducer {
+
+	@Value("${rabbitmq.exchange.name}")
+	private String exchange;
+
+	@Value("${rabbitmq.routing.key}")
+	private String routingKey;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMqProducer.class);
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public void sendMessage(String message) {
+		LOGGER.info(String.format("Message sent -> %s", message));
+		rabbitTemplate.convertAndSend(exchange, routingKey, message);
+	}
+
+}
